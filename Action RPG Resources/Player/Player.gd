b64a03e25b91da360bc01f384ea816067dcae2d9
Theofrid <theofridthiyadore@gmail.com>
2023-12-14 01:38:03 -0500

    - Changed move_and_collide with move_toward - Attached individual collider for player and collision polygon for the   boundaries

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -1,22 +1,22 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 200;
-const ACCELERATION = 50;
-const FRICTION = 400;
+const MAX_SPEED = 80;
+const ACCELERATION = 500;
+const FRICTION = 500;
 
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_raw_strength("ui_up")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		print(velocity)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
 
