28bdd583f43ffea750b7d38768836b0c4145138c
Theofrid <theofridthiyadore@gmail.com>
2023-12-17 21:38:58 -0500

    - Added Hurtbox for the bat and the associated functionalities

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -18,6 +18,7 @@ var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
@@ -33,12 +34,14 @@ func _physics_process(delta):
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO;
+	swordHitbox.knockback_vector = roll_vector
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
