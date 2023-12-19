1acd688320405a320c066b8b4cd3c233d54c19e3
Theofrid <theofridthiyadore@gmail.com>
2023-12-18 20:38:58 -0500

    - Used the get_parent() function instead of accessing the primary node   by using the get_tree().current_scene

--- Action RPG Resources/World/grass.gd
+++ Action RPG Resources/World/grass.gd
@@ -4,8 +4,7 @@ const GrassEffect = preload("res://Action RPG Resources/Effects/GrassEffect.tscn
 
 func create_grass_effect():
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
