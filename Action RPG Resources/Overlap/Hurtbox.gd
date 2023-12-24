5166e5a295e0f98fc54b3730a90c0257a65365c3
Theofrid <theofridthiyadore@gmail.com>
2023-12-24 16:59:00 -0500

    Hurtbox.gd - Replace the signal connection to show the hiteffect with the   create_hit_effect() function - Created signals (invincibility_started & invincibility_ended) and   developed set_invincible() function to emit signal inaccordance with   the invincible variable - Added a timeout signal to set invincible variable to false
    Bat.tscn
    - Attached the hitbox scene to the bat scene, set the collider shape
      to be a circle, and the collision mask was set to the PlayerHurtbox(3)

--- Action RPG Resources/Overlap/Hurtbox.gd
+++ Action RPG Resources/Overlap/Hurtbox.gd
@@ -1,13 +1,25 @@
 extends Area2D
 
-export(bool) var show_hit = true
-
 const HitEffect = preload("res://Action RPG Resources/Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit:
-		var effect = HitEffect.instance()
-		var main = get_tree().current_scene
-		main.add_child(effect)
-		effect.global_position = global_position
-		effect.queue_free()
+var invincible = false setget set_invincible
+
+signal invincibility_started
+signal invincibility_ended
+
+func set_invincible(value):
+	invincible = value
+	if invincible == true:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+func create_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+	effect.queue_free()
+	
+func _on_Timer_timeout():
+	self.invincible = false
