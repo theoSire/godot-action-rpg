f966c231dd5690676a67f3381a2f1a0bc01a6586
Theofrid <theofridthiyadore@gmail.com>
2023-12-23 23:01:13 -0500

    - Accessed the PlayerStats scene which could be globally accessed since   it was autoloaded - Signal connection was established in the func_ready() with the stats - knockback_vector of the sword was set

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -14,6 +14,7 @@ enum {
 var state = MOVE
 var velocity = Vector2.ZERO
 var roll_vector = Vector2.DOWN
+var stats = PlayerStats #Accesses the global scene (PlayerStats) which was autoloaded (Project/ProjectSettings/Autoload)
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -21,7 +22,9 @@ onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 func _physics_process(delta):
 	match state:
