12dc28b3cb332e1baed0f7fe5381ba2287156aeb
Theofrid <theofridthiyadore@gmail.com>
2023-12-24 18:37:52 -0500

    - Added a timer variable to access the Timer node - Created start_invinciblity() function to change the invincible   variable and sets the timer to a duration of the parameter - New signal connections were established   (_on_Hurtbox_invincibility_ended &   _on_Hurtbox_invincibility_started()) to change the monitoring property   of the scene

--- Action RPG Resources/Overlap/Hurtbox.gd
+++ Action RPG Resources/Overlap/Hurtbox.gd
@@ -4,15 +4,21 @@ const HitEffect = preload("res://Action RPG Resources/Effects/HitEffect.tscn")
 
 var invincible = false setget set_invincible
 
+onready var timer = $Timer
+
 signal invincibility_started
 signal invincibility_ended
 
 func set_invincible(value):
-	invincible = value
+	invincible = value # since self is not included, the setter is not called
 	if invincible == true:
 		emit_signal("invincibility_started")
 	else:
 		emit_signal("invincibility_ended")
+
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start(duration)
 
 func create_hit_effect():
 	var effect = HitEffect.instance()
@@ -22,4 +28,10 @@ func create_hit_effect():
 	effect.queue_free()
 	
 func _on_Timer_timeout():
-	self.invincible = false
+	self.invincible = false #Adding the self calls the setter
+
+func _on_Hurtbox_invincibility_ended():
+	monitoring = true
+
+func _on_Hurtbox_invincibility_started():
+	set_deferred("monitoring", false)
