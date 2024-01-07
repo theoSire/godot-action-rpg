c171a623e56d63ebb04fcc6e70b357e3ef69b72b
Theofrid <theofridthiyadore@gmail.com>
2024-01-06 23:20:50 -0500

    - The code for the preloading of the PlayerHurtSound node was added and   another variable was added to access the BlinkAnimationPlayer node - Unnecessary parameters were removed from the functions and the   velocity value was modified in the roll_animation_finished() function - In the _on_Hurtbox_area_entered() function, the health reduced by the   damage value, instead of a magic number (1), the   invincibility was increased to 0.6, and the player hurt sound was   added to it - Two signal connections (_on_Hurtbox_invincibility_started & _on_Hurtbox_invincibility_ended) were established to play different animations of the blinkAnimationPlayer node

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const PlayerHurtSound = preload("res://Action RPG Resources/Player/PlayerHurtSound.tscn")
+
 export var MAX_SPEED = 80;
 export var ROLL_SPEED = 115;
 export var ACCELERATION = 500;
@@ -21,6 +23,7 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
 onready var hurtbox = $Hurtbox
+onready var blinkAnimationPlayer = $BlinkAnimationPlayer
 
 func _ready():
 	randomize()
@@ -33,9 +36,9 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			roll_state(delta)
+			roll_state()
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO;
@@ -66,11 +69,11 @@ func move_state(delta):
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
-func attack_state(delta):
+func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 
-func roll_state(delta):
+func roll_state():
 	velocity = roll_vector * ROLL_SPEED
 	animationState.travel("Roll")
 	move()
@@ -79,13 +82,21 @@ func move():
 	velocity = move_and_slide(velocity)
 
 func roll_animation_finished():
+	velocity = velocity * 0.8
 	state = MOVE
 
 func attack_animation_finished():
 	state = MOVE
  
 func _on_Hurtbox_area_entered(area):
-	stats.health -= 1
-	hurtbox.start_invincibility(0.5)
+	stats.health -= area.damage
+	hurtbox.start_invincibility(0.6)
 	hurtbox.create_hit_effect()
-	print(stats.health)
+	var playerHurtSound = PlayerHurtSound.instance()
+	get_tree().current_scene.add_child(playerHurtSound)
+ 
+func _on_Hurtbox_invincibility_started():
+	blinkAnimationPlayer.play("Start")
+
+func _on_Hurtbox_invincibility_ended():
+	blinkAnimationPlayer.play("Stop")
